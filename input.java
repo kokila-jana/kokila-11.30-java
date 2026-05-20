f480202f3907d31ded7ac14e23de5f9710e7e4e1
kokila-jana <kokilagur2016@gmail.com>
2026-05-20 13:27:29 +0530

    Create input.java

---
+++ input.java
@@ -0,0 +1,223 @@
+package morning;
+import java.util.Scanner;
+public class input {
+
+	public static void main(String[] args)
+	{
+//		Scanner sc=new Scanner(System.in);
+//		System.out.println("Enter 2 numbers..");
+//		int a=sc.nextInt();
+//		int b=sc.nextInt();
+//		int c=a+b;
+//		System.out.println(c);
+//		byte x=sc.nextByte();
+		/*
+		 * nextByte();
+		 * nextShort()
+		 * nextLong()
+		 * nextFloat()
+		 * nextDouble()
+		 */
+		
+//		Scanner sc=new Scanner(System.in);
+////		String s=sc.next();
+//		String s=sc.nextLine();
+//		System.out.println(s);
+		
+//		Scanner sc=new Scanner(System.in);
+//		char c=sc.next().charAt(0);
+//		System.out.println(c);
+		
+//		Scanner sc=new Scanner(System.in);
+//		System.out.println("Enter 2 numbers..");
+//		int a=sc.nextInt();
+//		if(a%10==7  || a%7==0)
+//		{
+//			System.out.println("Buzz number..");
+//		}
+//		else
+//		{
+//			System.out.println("not Buzz number");
+//		}
+		
+//		Scanner sc=new Scanner(System.in);
+//		System.out.println("Enter 2 numbers..");
+//		int a=sc.nextInt();
+//		int b=a*a;
+//		int c=b%10;
+//		if (a==c)
+//		{
+//			System.out.println("AutoMorpic number");
+//		}
+//		else
+//		{
+//			System.out.println("not AutoMorpic number");
+//		}
+		
+//		for(int i=1;i<=255;i++)
+//		{
+//			System.out.println((char)i+" "+i);
+//		}
+		
+		
+//		Scanner sc=new Scanner(System.in);
+//		System.out.println("Enter the char..");
+//		char a=sc.next().charAt(0);
+//		if(a>='A' && a<='Z')
+//		{
+//			System.out.println("Capital letter");
+//		}
+//		else if(a>='a' && a<='z')
+//		{
+//			System.out.println("Small Letter");
+//		}
+//		else if(a>='0' && a<='9')
+//		{
+//			System.out.println("number");
+//		}
+//		else
+//		{
+//			System.out.println("special symbol");
+//		}
+//		
+		/*
+		 * if(con 1)
+		 * {
+		 * 
+		 * }
+		 * else if(condition 2)
+		 * {
+		 * 
+		 * }
+		 * else if(con 3)
+		 * {
+		 * 
+		 * }
+		 * .
+		 * .
+		 * .
+		 * else if(con n)
+		 * {
+		 * 
+		 * }
+		 * else
+		 * {
+		 * 
+		 * }
+		 * 
+		 * 
+		 */
+		
+		/*
+		 * nested if else
+		 * if(condition 1)
+		 * {
+		 * 		if(condition 2)
+		 * 		{
+		 * 
+		 * 		}
+		 * 		else
+		 * 		{
+		 * 	
+		 * 		}
+		 * 
+		 * }
+		 * else
+		 * {
+		 * 
+		 * }
+		 */
+		
+//		Scanner sc=new Scanner(System.in);
+//		System.out.println("Enter the number...");
+//		int a=sc.nextInt();
+//		if(a!=0)
+//		{
+//			if(a>0)
+//			{
+//				System.out.println("Positive number");
+//			}
+//			else
+//			{
+//				System.out.println("Negative number");
+//			}
+//			
+//		}
+//		else
+//		{
+//			System.out.println("Zero...");
+//		}
+		
+//		Scanner sc=new Scanner(System.in);
+//		System.out.println("Enter user name..");
+//		String name=sc.nextLine();
+//		if(name.equals("Java"))
+//		{
+//			System.out.println("Enter password..");
+//			String pass=sc.nextLine();
+//			if(pass.equals("12345"))
+//			{
+//				System.out.println("Login Success");
+//			}
+//			else
+//			{
+//				System.out.println("incorrect password");
+//			}
+//		}
+//		else
+//		{
+//			System.out.println("invalid user name");
+//		}
+		
+		/*
+		 * loop 2 types
+		 * 1.entry checked loop
+		 *   2 types
+		 *   1.for loop/countable loop
+		 *   2.while loop/infinity loop
+		 * 2.exit checked loop
+		 *   1.do-while loop
+		 *   
+		 *   for loop syntax
+		 *            1            2        4
+		 *   for(initialization;condition;inc/dec)
+		 *   {     3
+		 *   	//block of code
+		 *   }
+		 *   
+		 *   1.initialization
+		 *   2.condition
+		 *   3.block
+		 *   4.inc/dec
+		 * 
+		 */
+//		for(int i=1;i<=10;i++)
+//		{
+//			System.out.println(i);
+//		}
+		/*
+		 *     1       2        3      4
+		 *     i=1    1<=10     1      i=1+1=2
+		 *     i=2    2<=10     2      i=2+1=3
+		 *     .
+		 *     .
+		 *     .
+		 *     i=10  10<=10     10     i=10+1=11
+		 *     i=11  11<=10(false)
+		 */
+		
+//		for(int i=10;i>=1;i--)
+//		{
+//			System.out.println(i);
+//		}
+		
+		int i=1;
+		for(;i<=10;)
+		{
+			System.out.println(i);
+		}
+}
+		
+		
+
+}
