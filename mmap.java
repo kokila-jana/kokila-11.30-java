b0b7dd4f7094b76b37f041ffd611347ebea88928
kokila-jana <kokilagur2016@gmail.com>
2026-07-01 13:27:33 +0530

    Add initial implementation of mmap.java

---
+++ mmap.java
@@ -0,0 +1,78 @@
+TreeMap<Integer, String> h=new TreeMap<Integer, String>();
+////		h.put(14,"Java");
+////		h.put(45,"Python");
+////		h.put(78,"Fullstack");
+////		h.put(98,"Datascience");
+////		h.put(14,"JavaFS");
+////		h.put(23,"PowerBI");
+////		System.out.println(h);
+////		System.out.println(h.firstKey());
+////		System.out.println(h.lastKey());
+////		System.out.println(h.higherKey(23));
+////		System.out.println(h.lowerKey(45));
+////		System.out.println(h.firstEntry());
+////		System.out.println(h.lastEntry());
+////		System.out.println(h.pollFirstEntry());
+////		System.out.println(h.pollLastEntry());
+////		System.out.println(h);
+////		for(Map.Entry<Integer, String> e:h.entrySet())
+////		{
+////			System.out.println(e);
+////		}
+//		
+////		System.out.println(h.get(14));
+////		System.out.println(h.remove(98));
+////		System.out.println(h.replace(45, "Python", "PythonFS"));
+////		System.out.println(h.entrySet());
+////		System.out.println(h.keySet());
+////		System.out.println(h.values());
+////		System.out.println(h);
+////		h.compute(78,(key,val)->val.toUpperCase());
+////		System.out.println(h);
+////		h.computeIfPresent(23,(key,val)->val.concat(" java"));
+////		System.out.println(h);
+////		h.computeIfAbsent(7,val->"kokila");
+////		System.out.println(h);
+//		
+//		
+////		Stack<Object> s=new Stack<Object>();
+////		s.push(45);
+////		s.push(12.36);
+////		s.push("hello");
+////		s.push('f');
+////		s.push(false);
+////		s.push(12.36);
+////		System.out.println(s);
+////		System.out.println(s.capacity());
+////		System.out.println(s.elementAt(2));
+////		System.out.println(s.empty());
+////		System.out.println(s.firstElement());
+////		System.out.println(s.lastElement());
+////		System.out.println(s.peek());
+////		System.out.println(s.pop());
+////		System.out.println(s);
+////		System.out.println(s.search(45));
+////		System.out.println(s);
+//		
+//		
+//		
+//		Queue<Object> s=new LinkedList<Object>();
+//		s.add(45);
+//		s.add(12.36);
+//		s.add("hello");
+//		s.add('f');
+//		s.add(false);
+//		s.add(12.36);
+//		System.out.println(s);
+//		System.out.println(s.isEmpty());
+//		System.out.println(s.element());
+//		System.out.println(s.offer(56));
+//		System.out.println(s.remove());
+//		System.out.println(s);
+//		System.out.println(s);
+	}
+}
+
+
+
+
